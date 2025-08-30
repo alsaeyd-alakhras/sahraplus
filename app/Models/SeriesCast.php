50fbb7b17f21de6bc88b6b35c24630922c7acdc3
Bouthaina.Shamekh <33098191+Bouthaina-Shamekh@users.noreply.github.com>
2025-08-30 12:56:55 +0300

    m

--- app/Models/SeriesCast.php
+++ app/Models/SeriesCast.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SeriesCast extends Model
 {
@@ -26,7 +27,7 @@ class SeriesCast extends Model
 
     public function person()
     {
-        return $this->belongsTo(People::class);
+        return $this->belongsTo(Person::class);
     }
 
     // Scopes
